0ed23bcc5fde4552e1f9e122d2d43af471ff458e
刘亚东 <yadong.liu@husor.com>
2016-10-14 14:40:02 +0800

    first commit

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '7.0'
+
+target 'TwitterInstant' do
+  # Uncomment this line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+  pod 'ReactiveCocoa', '~>2.5.0'
+  # Pods for TwitterInstant
+
+end
---
+++ TwitterInstant/RWSearchFormViewController.m
@@ -0,0 +1,50 @@
+//
+//  RWSearchFormViewController.m
+//  TwitterInstant
+//
+//  Created by Colin Eberhardt on 02/12/2013.
+//  Copyright (c) 2013 Colin Eberhardt. All rights reserved.
+//
+
+#import "RWSearchFormViewController.h"
+#import "RWSearchResultsViewController.h"
+#import <ReactiveCocoa/ReactiveCocoa.h>
+
+@interface RWSearchFormViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *searchText;
+
+@property (strong, nonatomic) RWSearchResultsViewController *resultsViewController;
+
+@end
+
+@implementation RWSearchFormViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    self.title = @"Twitter Instant";
+
+    [self styleTextField:self.searchText];
+
+    self.resultsViewController = self.splitViewController.viewControllers[1];
+    
+    RAC(self.searchText,backgroundColor) = [self.searchText.rac_textSignal
+                                            map:^id(NSString *text) {
+                                                return [self isValidSearchText:text] ? [UIColor whiteColor] : [UIColor yellowColor];
+                                            }];
+}
+
+- (BOOL)isValidSearchText:(NSString *)text {
+    return text.length > 2;
+}
+
+- (void)styleTextField:(UITextField *)textField {
+  CALayer *textFieldLayer = textField.layer;
+  textFieldLayer.borderColor = [UIColor grayColor].CGColor;
+  textFieldLayer.borderWidth = 2.0f;
+  textFieldLayer.cornerRadius = 0.0f;
+}
+
+@end
